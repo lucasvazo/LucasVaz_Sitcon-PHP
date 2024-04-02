b9c22ade51557f4dd426954051a280602c5f8b65
amanzi-lucasvaz <lucas.vaz@amanzi.io>
2024-04-02 11:09:04 -0300

    feat: add agendamento

--- pages/solicitar.php
+++ pages/solicitar.php
@@ -1,11 +1,29 @@
 <?php 
     include 'config/connect.php';
 
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $profissionalId = $_POST['profissional'];
+        $tipoSolicitacaoId = $_POST['tipo_solicitacao'];
+        $procedimentoId = $_POST['procedimentos'];
+        $dataAgendamento = $_POST['data_agendamento'];
+        $horarioAgendamento = $_POST['horario_agendamento'];
+        $pacienteId = isset($_GET['id']) ? $_GET['id'] : 1;
+        $pacienteId = (int) $pacienteId;
+
+        $dataHoraAgendamento = $dataAgendamento . ' ' . $horarioAgendamento;
+
+        $query = "INSERT INTO agendamento (data_agendamento, paciente_id, procedimento_id, profissional_id)
+                  VALUES ('$dataHoraAgendamento', $pacienteId, $procedimentoId, $profissionalId)";
+    }
+
     $currentPatientId = isset($_GET['id']) ? $_GET['id'] : 1;
     $currentPatientId = (int) $currentPatientId;
 
     $currentProfissionalId = isset($_GET['profissional']) ? $_GET['profissional'] : 1;
     $currentProfissionalId = (int) $currentProfissionalId;
+
+    $currentTipoSolicitacaoId = isset($_GET['tipo_solicitacao']) ? $_GET['tipo_solicitacao'] : 1;
+    $currentTipoSolicitacaoId = (int) $currentTipoSolicitacaoId;
 
     $patientQuery = "SELECT * FROM paciente WHERE id = $currentPatientId";
     $patientResult = $connect->query($patientQuery);
@@ -14,8 +32,16 @@
     $profissionalQuery = "SELECT * FROM profissional WHERE status = 'ativo'";
     $profissionalResult = $connect->query($profissionalQuery);
 
-    $tipoSolicitacaoQuery = "SELECT * FROM tipo_solicitacao WHERE status = 'ativo'";
+    $tipoSolicitacaoQuery = "SELECT DISTINCT ts.id, ts.descricao, pa.profissional_id FROM tipo_solicitacao ts 
+                        INNER JOIN procedimento p ON ts.id = p.tipo_id 
+                        INNER JOIN profissional_atende pa ON p.id = pa.procedimento_id
+                        WHERE ts.status = 'ativo'";
     $tipoSolicitacaoResult = $connect->query($tipoSolicitacaoQuery);
+
+    $procedimentoQuery = "SELECT p.id, p.descricao, ts.id as tipo_id FROM procedimento p 
+                        INNER JOIN tipo_solicitacao ts ON p.tipo_id = ts.id 
+                        WHERE p.status = 'ativo'";
+    $procedimentoResult = $connect->query($procedimentoQuery);
 ?>
 
 <!DOCTYPE html>
@@ -74,11 +100,9 @@
                         <label for="profissional">Profissional *</label>
                         <select id="profissional" name="profissional" required>
                             <?php while ($profissional = $profissionalResult->fetch_assoc()) : ?>
-                                <?php if ($profissional['id'] == 1) : ?>
-                                    <option value="<?php echo $profissional['id']; ?>" selected><?php echo $profissional['nome']; ?></option>
-                                <?php else: ?>
-                                    <option value="<?php echo $profissional['id']; ?>"><?php echo $profissional['nome']; ?></option>
-                                <?php endif; ?>
+                                <option value="<?php echo $profissional['id']; ?>" <?php echo $profissional['id'] == $currentProfissionalId ? 'selected' : ''; ?>>
+                                    <?php echo $profissional['nome']; ?>
+                                </option>
                             <?php endwhile; ?>
                         </select>
                     </div>
@@ -88,8 +112,12 @@
                         <label for="tipo_solicitacao">Tipo de Solicitação *</label>
                         <select id="tipo_solicitacao" name="tipo_solicitacao" placeholder="Selecione o Tipo de Solicitação" required>
                             <option value="">Selecione o Tipo de Solicitação</option>  
-                            <?php while ($tipoSolicitacao = $tipoSolicitacaoResult->fetch_assoc()) : ?>
-                                <option value="<?php echo $tipoSolicitacao['id']; ?>"><?php echo $tipoSolicitacao['descricao']; ?></option>
+                            <?php 
+                            $tipoSolicitacaoResult->data_seek(0);
+                            while ($tipoSolicitacao = $tipoSolicitacaoResult->fetch_assoc()) : ?>
+                                <option value="<?php echo $tipoSolicitacao['id']; ?>" <?php echo $tipoSolicitacao['id'] == $currentTipoSolicitacaoId ? 'selected' : ''; ?>>
+                                    <?php echo $tipoSolicitacao['descricao']; ?>
+                                </option>
                             <?php endwhile; ?>
                         </select>
                     </div>
@@ -97,6 +125,16 @@
                         <label for="procedimentos">Procedimentos *</label>
                         <select id="procedimentos" name="procedimentos" required>
                             <option value="">Selecione os Procedimentos</option>
+                            <?php
+                                $procedimentoQueryFiltered = "SELECT p.id, p.descricao FROM procedimento p 
+                                WHERE p.status = 'ativo' AND p.tipo_id = $currentTipoSolicitacaoId";
+                                $procedimentoResultFiltered = $connect->query($procedimentoQueryFiltered);
+                                while ($procedimento = $procedimentoResultFiltered->fetch_assoc()) :
+                            ?>
+                                <option value="<?php echo $procedimento['id']; ?>">
+                                    <?php echo $procedimento['descricao']; ?>
+                                </option>
+                            <?php endwhile; ?>
                         </select>
                     </div>
                 </div>
@@ -117,4 +155,16 @@
         </main>
         <?php include 'includes/footer.php' ?>
     </body>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            const profissionalDropdown = document.getElementById('profissional');
+            const tipoSolicitacaoDropdown = document.getElementById('tipo_solicitacao');
+            profissionalDropdown.addEventListener('change', function() {
+                window.location.href = `?id=<?php echo $currentPatientId ?>&profissional=${this.value}`;
+            });
+            tipoSolicitacaoDropdown.addEventListener('change', function() {
+                window.location.href = `?id=<?php echo $currentPatientId ?>&profissional=<?php echo $currentProfissionalId ?>&tipo_solicitacao=${this.value}`;
+            });
+        });
+    </script>
 </html>
