b03d4443d9e8ce5dff1af6c53872eb32f153e88b
amanzi-lucasvaz <lucas.vaz@amanzi.io>
2024-04-02 10:19:02 -0300

    feat: list agendamentos

--- pages/solicitacoes.php
+++ pages/solicitacoes.php
@@ -1,12 +1,40 @@
+<?php 
+    include 'config/connect.php';
+    $itemsPerPage = 10;
+    $currentPage = isset($_GET['page']) ? $_GET['page'] : 1;
+    $offset = ($currentPage - 1) * $itemsPerPage;
+
+    $query = "SELECT agendamento.id, paciente.nome AS nome_paciente, paciente.cpf, tipo_solicitacao.descricao AS tipo_solicitacao, 
+                procedimento.descricao AS procedimentos, agendamento.data_agendamento
+                FROM agendamento
+                INNER JOIN paciente ON agendamento.paciente_id = paciente.id
+                INNER JOIN tipo_solicitacao ON agendamento.procedimento_id = tipo_solicitacao.id
+                INNER JOIN procedimento ON agendamento.procedimento_id = procedimento.id";
+          
+    $result = $connect->query($query);
+
+    if (!$result) {
+        trigger_error('Invalid query: ' . $connect->error);
+    }
+
+    $totalItems = $connect->query("SELECT COUNT(*) as total FROM agendamento")->fetch_assoc()['total'];
+    $totalPages = ceil($totalItems / $itemsPerPage);
+
+    if ($currentPage < 1) {
+        $currentPage = 1;
+    } elseif ($currentPage > $totalPages) {
+        $currentPage = $totalPages;
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lista de Solicitações</title>
+    <title>Agendamentos</title>
     <meta name="author" content="Lucas Vaz">
-    <link rel="icon" href="./assets/images/sitcon-logo.ico">
     <link rel="icon" href="./assets/images/sitcon-logo.ico">
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" 
@@ -20,11 +48,36 @@
     <!-- Styles -->
     <link rel="stylesheet" href="assets/styles/reset.css" />
     <link rel="stylesheet" href="assets/styles/global.css" />
-    <link rel="stylesheet" href="assets/styles/styles.css" />
+    <link rel="stylesheet" href="assets/styles/agendamentos.css" />
 </head>
 <body class="page-content">
     <?php include 'includes/header.php' ?>
     <main class="container main-content">
+        <table class="table agendamentos-table box-shadow">
+            <thead> 
+                <tr>
+                    <th scope="col">Nome Paciente</th>
+                    <th scope="col">CPF</th>
+                    <th scope="col">Tipo de Solicitação</th>
+                    <th scope="col">Procedimento</th>
+                    <th scope="col">Data e Hora</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    while ($row = $result-> fetch_assoc()) {
+                        echo "<tr scope='row' >";
+                        echo "<td>" . $row['nome_paciente'] . "</td>";
+                        echo "<td>" . $row['cpf'] . "</td>";
+                        echo "<td>" . $row['tipo_solicitacao'] . "</td>";
+                        echo "<td>" . $row['procedimentos'] . "</td>";
+                        echo "<td>" . date('d/m/Y H:i', strtotime($row['data_agendamento'])) . "</td>";
+                        echo "</tr>";
+                    }
+                ?>
+            </tbody>
+        </table>
+        <?php include 'includes/pagination.php' ?>
     </main>
     <?php include 'includes/footer.php' ?>
 </body>
